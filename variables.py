eefc8c9380c1f6098b774f8e17a73f97734bce61
Francesco Immorlano <francesco.immorlano@unisalento.it>
2024-12-10 20:11:05 -0800

    Add files via upload

--- variables.py
+++ variables.py
@@ -8,8 +8,9 @@ import numpy as np
 demo_download = False
 demo_no_download = False
 
-# Set to True if a figure or table must be computed
-compute_figures_tables = True
+# Set to True if a figure or table of the paper must be computed
+# Set False if you performed other Experiments that were saved in Experiments folder
+compute_figures_tables_paper = True
 
 ts = datetime.now()
 ts_human = ts.strftime('%Y-%m-%d_%H-%M-%S')
@@ -17,16 +18,21 @@ ts_human = ts.strftime('%Y-%m-%d_%H-%M-%S')
 variable = 'near_surface_air_temperature'
 variable_short = 'tas'
 
+# Number of degrees of freedom of smoothing splines
+n_dof = 20
+
 # Avg global surface temperature in 1995–2014
 global_mean_temp_1995_2014 = 14.711500000000001
 # Avg global surface temperature in 1850-1900
 global_mean_temp_1850_1900 = 13.798588235294114
 # Avg global surface temperature in 1850-2000
 global_mean_temp_1850_2000 = 14.007112582781458
-total_earth_area = 5.1009974e+14
+total_earth_area = 5.1009974e+14 # m^2
 
-if compute_figures_tables: path_area_cella = '../area_cella.csv'
-else: path_area_cella = './area_cella.csv'
+if demo_download or demo_no_download:
+  path_area_cella = './area_cella.csv'
+else:
+  path_area_cella = '../area_cella.csv'
 
 with open(path_area_cella, newline='') as csvfile:
     area_cella = np.genfromtxt(csvfile, delimiter=',')
@@ -46,7 +52,8 @@ CO2eq_climate_model = 'MCE-v1-2'
 withAerosolForcing = True
 
 lr_first_train = 1e-4
-lr_tl = 1e-5
+lr_loo_cv = 0.25e-5
+lr_tl_obs = 1e-5
 
 loss = 'mae'
 
@@ -56,9 +63,16 @@ batch_size_tl = 16
 
 first_ssp_year = 2015
 
+# Set window size for computing moving avg
+window_size = 18
+
 # Min and max CMIP6 temperature values
 y_min = 212.1662
 y_max = 317.38766
+
+# Min and max CO2eq values
+X_min = 285.1337
+X_max = 1262.1862
 
 # settings for modern reference time period and proxy for pre-industrial time period
 refperiod_start = 1995
@@ -74,7 +88,8 @@ refperiod_conversion = 0.85
 '''
 start_year_first_training = 1850
 end_year_first_training = 2098
-val_year_first_training = 2095
+start_year_first_training_val = 2070
+end_year_first_training_val = 2080
 n_training_years_first_training = end_year_first_training-start_year_first_training+1
 
 '''
@@ -200,13 +215,10 @@ else:
   models_list = models_list_complete
   models_short_list = models_short_list_complete
   short_scenarios_list = short_scenarios_list_complete
-  if compute_figures_tables: 
-    ROOT_EXPERIMENTS = '../Experiments'
-    ROOT_SOURCE_DATA = '../Source_data'
-  else: 
-    ROOT_EXPERIMENTS = './Experiments'
-    ROOT_SOURCE_DATA = './Source_data'
+  ROOT_EXPERIMENTS = '../Experiments'
+  ROOT_SOURCE_DATA = '../Source_data'
 
+PATH_SMOOTHED_CMIP6_SIMULATIONS_DIRECTORY = f'{ROOT_SOURCE_DATA}/CMIP6_data/near_surface_air_temperature'
 PATH_ANNUAL_SIMULATIONS_DIRECTORY = f'{ROOT_SOURCE_DATA}/CMIP6_data/near_surface_air_temperature/Annual_uniform_remapped'
 PATH_BEST_DATA = f'{ROOT_SOURCE_DATA}/BEST_data/BEST_regridded_annual_1979-2022.nc'
 PATH_BEST_DATA_UNCERTAINTY = f'{ROOT_SOURCE_DATA}/BEST_data/Land_and_Ocean_global_average_annual.txt'
